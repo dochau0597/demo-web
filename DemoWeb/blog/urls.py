3150c9778d2b86db10f6db8b1767c143721d2beb
dochau0597 <dochau0597@outlook.com>
2020-02-19 07:13:52 +0700

    them nhieu thu

--- DemoWeb/blog/urls.py
+++ DemoWeb/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/lienhe', views.lienhe, name='lienhe'),    
-    path('/login', views.login, name='login'),    
+    path('lienhe', views.lienhe, name='lienhe'),    
+    path('login', views.login, name='login'),    
+    path('baiviet', views.baiviet, name='baiviet'),    
 ]
--- DemoWeb/blog/views.py
+++ DemoWeb/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
 def index(request):
@@ -10,3 +10,7 @@ def lienhe(request):
 
 def login(request):
     return render(request, 'login.html')
+
+def baiviet(request):
+    Data = {'Posts':  Post.objects.all().order_by("-date")}
+    return render(request, 'baiviet.html', Data)
